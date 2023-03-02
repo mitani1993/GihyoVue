3e7bac199e22a082af394238818fd76041a792bd
mitani1993 <69582233+mitani1993@users.noreply.github.com>
2023-03-02 20:32:30 +0900

    リスト3-5

--- chap03/reactive-function/src/App.vue
+++ chap03/reactive-function/src/App.vue
@@ -1,47 +1,26 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { reactive, computed } from "vue";
+
+// リアクティブなテンプレート変数をまとめて用意。
+const data = reactive({
+  PI: 3.14,
+  radius: Math.round(Math.random() * 10)
+});
+// 円の面積の算出プロパティを用意。
+const area = computed(
+  (): number => {
+    return data.radius * data.radius * data.PI;
+  }
+);
+// 半径のテンプレート変数に新しい乱数を1秒ごとに格納。
+setInterval(
+  (): void => {
+    data.radius = Math.round(Math.random() * 10);
+  },
+  1000
+);
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <p>半径{{ data.radius }}の円の面積を円周率{{ data.PI }}で計算すると、{{ area }}</p>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
