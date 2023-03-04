90043930d0fbe6311fd4c9dab623efb8b373b145
mitani1993 <fytim55@gmail.com>
2023-03-04 21:16:16 +0900

    リスト4-8

--- chap04/directive-on-param-event/src/App.vue
+++ chap04/directive-on-param-event/src/App.vue
@@ -1,47 +1,17 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { ref } from "vue";
+
+const mousePointerX = ref(0);
+const mousePointerY = ref(0);
+const onImgMousemove = (event: MouseEvent): void => {
+  mousePointerX.value = event.offsetX;
+  mousePointerY.value = event.offsetY;
+};
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <section>
+    <img src="./assets/logo.svg" alt="Vueのロゴ" width="200" v-on:mousemove="onImgMousemove">
+    <p>ポインタの位置: x={{ mousePointerX }} y={{ mousePointerY }}</p>
+  </section>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
