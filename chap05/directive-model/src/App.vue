22f015d2c9702ad4406fa2b4581f3d53de89d6ad
mitani1993 <69582233+mitani1993@users.noreply.github.com>
2023-03-05 21:10:50 +0900

    リスト5-1

--- chap05/directive-model/src/App.vue
+++ chap05/directive-model/src/App.vue
@@ -1,4 +1,12 @@
 <script setup lang="ts">
+import { ref } from 'vue';
+
+const inputNameModel = ref("双方向");
 </script>
 
-<template></template>
+<template>
+  <section>
+    <input type="text" v-model="inputNameModel">
+    <p>{{ inputNameModel }}</p>
+  </section>
+</template>
