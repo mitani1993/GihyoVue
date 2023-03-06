8e0a97d94fdcab789f1ad79239051bc0a3e56306
mitani1993 <69582233+mitani1993@users.noreply.github.com>
2023-03-06 10:26:38 +0900

    リスト5-8

--- chap05/directive-once/src/App.vue
+++ chap05/directive-once/src/App.vue
@@ -1,4 +1,13 @@
 <script setup lang="ts">
+import { ref } from "vue";
+
+const price = ref(1000);
 </script>
 
-<template></template>
+<template>
+  <section>
+    <input type="number" v-model="price">円<br>
+    <p>金額は{{ price }}円です。</p>
+    <p v-once>金額は{{ price }}円です。</p>
+  </section>
+</template>
