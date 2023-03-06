26b419862bdeaa4b2b12ece9fcc0909379755712
mitani1993 <69582233+mitani1993@users.noreply.github.com>
2023-03-06 10:31:17 +0900

    リスト5-9

--- chap05/directive-cloak/src/App.vue
+++ chap05/directive-cloak/src/App.vue
@@ -1,4 +1,15 @@
 <script setup lang="ts">
+import { ref } from "vue";
+
+const hello = ref("こんにちは!");
 </script>
 
-<template></template>
+<template>
+  <p v-cloak>{{ hello }}</p>
+</template>
+
+<style>
+[v-cloak] {
+  display: none;
+}
+</style>
