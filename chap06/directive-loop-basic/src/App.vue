9c519b4bcabbff46bce8d6bccedc673452be0a72
mitani1993 <fytim55@gmail.com>
2023-03-06 16:57:17 +0900

    リスト6-5

--- chap06/directive-loop-basic/src/App.vue
+++ chap06/directive-loop-basic/src/App.vue
@@ -1,5 +1,23 @@
 <script setup lang="ts">
+import { ref } from "vue";
+
+const cocktailListInit: string[] = ["ホワイトレディ", "ブルーハワイ", "ニューヨーク"];
+const coctailList = ref(cocktailListInit);
 </script>
 
 <template>
+  <ul>
+    <li
+      v-for="coctailName in coctailList"
+      v-bind:key="coctailName">
+      {{ coctailName }}
+    </li>
+  </ul>
+  <ul>
+    <li
+      v-for="(coctailName, index) in coctailList"
+      v-bind:key="coctailName">
+      {{ coctailName }}(インデックス{{ index }})
+    </li>
+  </ul>
 </template>
