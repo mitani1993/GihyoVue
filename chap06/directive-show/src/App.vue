b057bd18dd04c7be4eafa6d06d03de9171f19f24
mitani1993 <fytim55@gmail.com>
2023-03-06 16:40:13 +0900

    リスト6-4

--- chap06/directive-show/src/App.vue
+++ chap06/directive-show/src/App.vue
@@ -1,5 +1,28 @@
 <script setup lang="ts">
+import { computed, ref } from "vue";
+
+const showOrNot = computed(
+  (): boolean => {
+    // 戻り値用の変数を初期値falseで用意。
+    let showOrNot = false;
+    // 0~100の乱数を発生;
+    const rand = Math.round(Math.random() * 100);
+    // 乱数が50以上ならば、戻り値をtrueに変更;
+    if (rand >= 50) {
+      showOrNot = true;
+    }
+    return showOrNot;
+  }
+);
 </script>
 
 <template>
+  <section>
+    v-ifを利用
+    <p v-if="showOrNot">条件に合致したので表示</p>
+  </section>
+  <section>
+    v-showを利用
+    <p v-show="showOrNot">条件に合致したので表示</p>
+  </section>
 </template>
