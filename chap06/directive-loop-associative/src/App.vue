8c87694b6527517061f4e67ce59ce84aae7aa8c0
mitani1993 <fytim55@gmail.com>
2023-03-06 17:08:22 +0900

    リスト6-6

--- chap06/directive-loop-associative/src/App.vue
+++ chap06/directive-loop-associative/src/App.vue
@@ -1,5 +1,27 @@
 <script setup lang="ts">
+import { ref } from "vue";
+
+const coctailListInit: {[key: number]: string} = {
+  2345: "ホワイトレディ",
+  4412: "ブルーハワイ",
+  6792: "ニューヨーク"
+};
+const coctailList = ref(coctailListInit);
 </script>
 
 <template>
+  <ul>
+    <li
+      v-for="(coctailName, id) in coctailList"
+      v-bind:key="'coctailList' + id">
+      IDが{{ id }}のカクテルは{{ coctailName }}
+    </li>
+  </ul>
+  <ul>
+    <li
+      v-for="(coctailName, id, index) in coctailList"
+      v-bind:key="'coctailListWithIdx' + id">
+      {{ index + 1 }}: IDが{{ id }}のカクテルは{{ coctailName }}
+    </li>
+  </ul>
 </template>
