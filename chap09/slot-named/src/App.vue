36c702f4326d9f6bacd96858855260918d246630
mitani1993 <69582233+mitani1993@users.noreply.github.com>
2023-03-09 14:46:18 +0900

    リスト9-9

--- chap09/slot-named/src/App.vue
+++ chap09/slot-named/src/App.vue
@@ -1,4 +1,25 @@
 <script setup lang="ts">
+import { ref } from "vue";
+import OneSection from "./components/OneSection.vue";
+
+const taroProblemsInt: string[] = ["電話が通じません。", "留守です。"];
+const taro = ref("田中太郎");
+const taroProblems = ref(taroProblemsInt);
+const jiro = ref("鈴木二郎");
 </script>
 
-<template></template>
+<template>
+  <section>
+    <OneSection v-bind:name="taro">
+      <template v-slot:default>
+        <p>問題発生</p>
+      </template>
+      <template v-slot:detail>
+        <li v-for="problem in taroProblems" v-bind:key="problem">
+          {{ problem }}
+        </li>
+      </template>
+    </OneSection>
+    <OneSection v-bind:name="jiro" />
+  </section>
+</template>
