f74dbc96da0495c8cd00047c3ada220e22789826
mitani1993 <69582233+mitani1993@users.noreply.github.com>
2023-03-09 15:18:09 +0900

    slotの受け取り方

--- chap09/slot-scoped/src/App.vue
+++ chap09/slot-scoped/src/App.vue
@@ -14,5 +14,32 @@ import OneSection from './components/OneSection.vue';
         </dl>
       </template>
     </OneSection>
+
+    <!-- default省略 -->
+    <OneSection>
+      <template v-slot="slotProps">
+        <dl>
+          <dt>名前</dt>
+          <dd>{{ slotProps.memberInfo.name }}</dd>
+          <dt>状況</dt>
+          <dd>{{ slotProps.memberInfo.state }}</dd>
+        </dl>
+      </template>
+    </OneSection>
+
+    <!-- 分割代入 -->
+    <OneSection>
+      <template v-slot="{ memberInfo }">
+        <dl>
+          <dt>名前</dt>
+          <dd>{{ memberInfo.name }}</dd>
+          <dt>状況</dt>
+          <dd>{{ memberInfo.state }}</dd>
+        </dl>
+      </template>
+    </OneSection>
+
+    <!-- フォールバックコンテンツ -->
+    <OneSection />
   </section>
 </template>
