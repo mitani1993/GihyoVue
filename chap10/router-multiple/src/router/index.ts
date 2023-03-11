d100e78c94347fe8f7aff2eac59315f99c98399c
mitani1993 <fytim55@gmail.com>
2023-03-11 14:24:49 +0900

    リスト10-20

--- chap10/router-multiple/src/router/index.ts
+++ chap10/router-multiple/src/router/index.ts
@@ -1,14 +1,22 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from "vue-router";
+import type { RouteRecordRaw } from "vue-router";
+import AppTop from "@/views/AppTop.vue";
+import SubPage from "@/views/SubPage.vue";
+
+const routeSettings: RouteRecordRaw[] = [
+  {
+    path: "/",
+    name: "AppTop",
+    components: {
+      default: AppTop,
+      sub: SubPage
+    }
+  }
+]
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-  ]
+  routes: routeSettings
 })
 
 export default router
