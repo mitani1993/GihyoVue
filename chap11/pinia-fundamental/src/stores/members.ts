b32af6bd5dd962b9437f946b7238cc4436abfa8e
mitani1993 <fytim55@gmail.com>
2023-03-12 11:03:10 +0900

    リスト11-4

---
+++ chap11/pinia-fundamental/src/stores/members.ts
@@ -0,0 +1,23 @@
+import { defineStore } from "pinia"
+import type { Member } from "@/interfaces"
+
+interface State {
+  memberList: Map<number, Member>;
+}
+
+export const useCounterStore = defineStore({
+  id: "member",
+  state: (): State => {
+    return {
+      memberList: new Map<number, Member>()
+    };
+  },
+  getters: {
+  },
+  actions: {
+    initList(): void {
+      this.memberList.set(33456, { id: 33456, name: "田中太郎", email: "bow@example.com", points: 35, note: "初回入会特典あり。" });
+      this.memberList.set(47783, { id: 47783, name: "鈴木二郎", email: "mue@example.com", points: 53 });
+    },
+  }
+});
