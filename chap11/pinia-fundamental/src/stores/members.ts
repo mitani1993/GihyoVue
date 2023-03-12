bc4306a8d92bcbad988baa88609bcdf091a57788
mitani1993 <fytim55@gmail.com>
2023-03-12 11:18:04 +0900

    リスト11-10

--- chap11/pinia-fundamental/src/stores/members.ts
+++ chap11/pinia-fundamental/src/stores/members.ts
@@ -25,5 +25,8 @@ export const useMemberStore = defineStore({
       this.memberList.set(33456, { id: 33456, name: "田中太郎", email: "bow@example.com", points: 35, note: "初回入会特典あり。" });
       this.memberList.set(47783, { id: 47783, name: "鈴木二郎", email: "mue@example.com", points: 53 });
     },
+    addMember(member: Member): void {
+      this.memberList.set(member.id, member);
+    }
   }
 });
