6f44370dfda47670d58aabbda1fdd3ca35daa752
mitani1993 <fytim55@gmail.com>
2023-03-12 10:36:50 +0900

    リスト11-2

--- chap11/pinia-basic/src/stores/counter.ts
+++ chap11/pinia-basic/src/stores/counter.ts
@@ -1,12 +1,24 @@
-import { ref, computed } from 'vue'
-import { defineStore } from 'pinia'
+import {defineStore} from "pinia";
 
-export const useCounterStore = defineStore('counter', () => {
-  const count = ref(0)
-  const doubleCount = computed(() => count.value * 2)
-  function increment() {
-    count.value++
+interface State {
+  counter: number;
+}
+
+export const useCounterStore = defineStore({
+  id: "counter",
+  state: (): State => {
+    return {
+      counter: 0
+    };
+  },
+  getters: {
+    doubleCount: (state): number => {
+      return state.counter * 2;
+    }
+  },
+  actions: {
+    incrementCount(): void {
+      this.counter++;
+    }
   }
-
-  return { count, doubleCount, increment }
-})
+});
