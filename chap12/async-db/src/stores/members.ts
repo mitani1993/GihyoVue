900309ff508c70e248617714249f788739746d5c
mitani1993 <fytim55@gmail.com>
2023-03-14 15:41:24 +0900

    リスト12-6

--- chap12/async-db/src/stores/members.ts
+++ chap12/async-db/src/stores/members.ts
@@ -10,7 +10,7 @@ let _database: IDBDatabase;
 async function getDatabase(): Promise<IDBDatabase> {
   const promise = new Promise<IDBDatabase>(
     (resolve, reject): void => {
-      if (_database =! undefined) {
+      if (_database != undefined) {
         resolve(_database);
       } else {
         const request = window.indexedDB.open("asyncdb", 1);
@@ -54,20 +54,54 @@ export const useMemberStore = defineStore({
     }
   },
   actions: {
-    prepareMemberList(): void {
-      // 空のmemberListを用意。
-      let memberList = new Map<number, Member>();
-      // セッションストレージからデータを取得。
-      const memberListJSONStr = sessionStorage.getItem("memberList");
-      // セッションストレージのデータが空でないなら
-      if (memberListJSONStr != undefined) {
-        // JSON文字列をJSONオブジェクトに変換。
-        const memberListJSON = JSON.parse(memberListJSONStr);
-        // JSONオブジェクトを元にmemberListを生成。
-        memberList = new Map<number, Member>(memberListJSON);
-      }
-      // ステートにmemberListを格納。
-      this.memberList = memberList;
+    async prepareMemberList(): Promise<boolean> {
+      // データベースオブジェクトを取得する。
+      const database = await getDatabase();
+      const promise = new Promise<boolean>(
+        (resolve, reject) => {
+          // トランザクションオブジェクトを取得する。
+          const transaction = database.transaction("members", "readonly");
+          // membersオブジェクトストアを取得する。
+          const objectStore = transaction.objectStore("members");
+          // 空のmemberListを生成。
+          const memberList = new Map<number, Member>();
+          // membersオブジェクトストアの全データを取得。
+          const request = objectStore.openCursor();
+          // データ取得が成功した場合の処理を登録。
+          request.onsuccess = (event) => {
+            // カーソルオブジェクトを取得。
+            const target = event.target as IDBRequest;
+            const cursor = target.result as IDBCursorWithValue;
+            // カーソルが存在すれば..
+            if (cursor) {
+              // カーソルからキーデータを取得。
+              const id = cursor.key as number;
+              // カーソル空値オブジェクトを取得。
+              const member = cursor.value as Member;
+              // memberListに格納。
+              memberList.set(id, member);
+              // 次のデータに同じ処理を実行。
+              cursor.continue();
+            }
+          };
+          // トランザクションが成功した場合の処理を登録。
+          transaction.oncomplete = () => {
+            // ステートにmemberListを格納。
+            this.memberList = memberList;
+            // ステートのisLoadingをfalseに変更。
+            this.isLoading = false;
+            // 非同期処理成功。 Promise内の戻り値をtrueに。
+            resolve(true);
+          };
+          // トランザクションが失敗した場合の処理を登録。
+          transaction.onerror = (event) => {
+            // 非同期処理失敗。 エラーメッセージを格納。
+            console.log("ERROR: データ取得に失敗。", event);
+            reject(new Error("ERROR: データ取得に失敗。"));
+          };
+        }
+      );
+      return promise;
     },
     insertMember(member: Member): void {
       // ステートのmemberListに引数の会員情報を追加。
